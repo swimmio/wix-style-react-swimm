3a5a27a1d6e107ecfa0709c1a298794cadefb2fc
Egozi <egozi@wix.com>
2020-08-16 21:02:33 +0300

    <LineChart/> - POC

--- src/LineChart/test/LineChart.spec.js
+++ src/LineChart/test/LineChart.spec.js
@@ -7,31 +7,5 @@ import { lineChartPrivateDriverFactory } from './LineChart.private.uni.driver';
 describe(LineChart.displayName, () => {
   const render = createRendererWithUniDriver(lineChartPrivateDriverFactory);
 
-  afterEach(() => {
-    cleanup();
-  });
-
-  it('should render', async () => {
-    const { driver } = render(<LineChart />);
-
-    expect(await driver.exists()).toBe(true);
-    expect(await driver.getButtonText()).toEqual('Click me!');
-  });
-
-  it('should increment', async () => {
-    const { driver } = render(<LineChart />);
-
-    await driver.clickButton();
-    await driver.clickButton();
-
-    expect(await driver.getCountText()).toEqual(
-      'You clicked this button even number (2) of times',
-    );
-  });
-
-  it('should allow changing the button text', async () => {
-    const { driver } = render(<LineChart buttonText="Press me" />);
-
-    expect(await driver.getButtonText()).toEqual('Press me');
-  });
+  afterEach(cleanup);
 });
