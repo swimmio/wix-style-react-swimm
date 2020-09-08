e649657636e9eef426cb5cc3447209773893a4d8
Eyal Eizenberg <eyale@wix.com>
2020-09-08 19:24:39 +0300

    Types for FontUpgrade driver fix (#5949)

--- src/FontUpgrade/FontUpgrade.uni.driver.d.ts
+++ src/FontUpgrade/FontUpgrade.uni.driver.d.ts
@@ -2,5 +2,5 @@ import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
 
 export interface FontUpgradeUniDriver extends BaseUniDriver {
   getElement(selector: string): Promise<any>;
-  isActive: Promise<boolean>;
+  isActive(): Promise<boolean>;
 }
