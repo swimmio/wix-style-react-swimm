756af179598d0f33a8d0688b51867630c31916d0
Audrius Siliunas <audriuss@wix.com>
2020-09-21 14:49:39 +0300

    <RadioGroup/> - added RadioButtonDriver types to getRadioAtIndex driver method (#5981)

--- src/RadioGroup/RadioGroup.driver.d.ts
+++ src/RadioGroup/RadioGroup.driver.d.ts
@@ -1,10 +1,11 @@
 import { BaseDriver } from 'wix-ui-test-utils/driver-factory';
+import { RadioButtonDriver } from './RadioButton/RadioButton.driver';
 
 export interface RadioGroupDriver extends BaseDriver {
   selectByValue: (value: string | number) => void;
   selectByIndex: (index: number) => void;
   getRadioValueAt: (index: number) => string | number;
-  getRadioAtIndex: (index: number) => HTMLElement;
+  getRadioAtIndex: (index: number) => HTMLElement & RadioButtonDriver;
   getSelectedValue: () => string | number | null;
   isRadioDisabled: (index: number) => boolean;
   getClassOfLabelAt: (index: number) => String;
--- src/RadioGroup/RadioGroup.uni.driver.d.ts
+++ src/RadioGroup/RadioGroup.uni.driver.d.ts
@@ -1,4 +1,5 @@
 import { BaseUniDriver } from 'wix-ui-test-utils/unidriver';
+import { RadioButtonUniDriver } from './RadioButton/RadioButton.uni.driver';
 
 export function createRadioButtonsGetter(
   base: BaseUniDriver,
@@ -9,7 +10,9 @@ export interface RadioGroupUniDriver extends BaseUniDriver {
   selectByValue: (value: string | number) => Promise<void>;
   selectByIndex: (index: number) => Promise<void>;
   getRadioValueAt: (index: number) => Promise<string | number>;
-  getRadioAtIndex: (index: number) => Promise<HTMLElement>;
+  getRadioAtIndex: (
+    index: number,
+  ) => Promise<HTMLElement & RadioButtonUniDriver>;
   getSelectedValue: () => Promise<string | number | null>;
   isRadioDisabled: (index: number) => Promise<boolean>;
   getClassOfLabelAt: (index: number) => Promise<String>;
