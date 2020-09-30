e33b323fe005a9f4bc86927d2985351d7a914ab9
Marijus Siliunas <marijuss@wix.com>
2020-09-30 13:47:34 +0300

    <AddressInput /> - Update driver

--- src/AddressInput/AddressInput.uni.driver.js
+++ src/AddressInput/AddressInput.uni.driver.js
@@ -11,11 +11,12 @@ export const addressInputDriverFactory = (base, body) => {
   return {
     ...baseUniDriverFactory(base, body),
 
-    getInputValue: inputDriver.getValue,
-    enterText: inputDriver.enterText,
-    clearText: inputDriver.clearText,
-    isDisabled: driver.isDisabled,
-    clickAtOption: dropdownLayoutDriver.clickAtOption,
-    clickAtOptionWithValue: dropdownLayoutDriver.clickAtOptionWithValue,
+    getInputValue: () => inputDriver.getValue(),
+    enterText: text => inputDriver.enterText(text),
+    clearText: () => inputDriver.clearText(),
+    isDisabled: () => driver.isDisabled(),
+    clickAtOption: position => dropdownLayoutDriver.clickAtOption(position),
+    clickAtOptionWithValue: value =>
+      dropdownLayoutDriver.clickAtOptionWithValue(value),
   };
 };
