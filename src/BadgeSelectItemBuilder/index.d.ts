c575cb477de728ccefc55f7ca54866e5b85ee924
bendetro <ronben@wix.com>
2020-10-06 15:44:49 +0300

    Updated type

--- src/BadgeSelectItemBuilder/index.d.ts
+++ src/BadgeSelectItemBuilder/index.d.ts
@@ -3,6 +3,7 @@ import * as React from 'react';
 export type BadgeSelectItemBuilderFn = (data: {
   id: string | number;
   text: React.ReactNode;
+  subtitle?: string;
   skin: BadgeSelectItemSkin;
 }) => {
   id: string | number;
