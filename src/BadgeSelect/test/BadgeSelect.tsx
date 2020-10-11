80d741b48f792a863cb91544bc6ab4e8481c9f32
Ron Bendet <42215057+bendetro@users.noreply.github.com>
2020-10-11 15:46:51 +0300

     <BadgeSelect/> - Add subtitle to BadgeSelectOptionType (#6074)
    * WSR | Add subtitle to BadgeSelectOptionType
    
    * Add subtitle to BadgeSelectOption
    
    * Refactor

--- src/BadgeSelect/test/BadgeSelect.tsx
+++ src/BadgeSelect/test/BadgeSelect.tsx
@@ -29,7 +29,7 @@ function BadgeSelectWithAllProps() {
       type="outlined"
       dataHook="hook"
       onSelect={o => undefined}
-      options={[{ id: '1', skin: 'danger', text: 'text' }]}
+      options={[{ id: '1', skin: 'danger', text: 'text', subtitle: 'subtitle' }]}
       selectedId="1"
       size="medium"
       uppercase
@@ -40,7 +40,7 @@ function BadgeSelectWithAllProps() {
 
 function testInstanceMethods() {
   const instance = new BadgeSelect({
-    options: [{ id: '1', skin: 'danger', text: 'text' }],
+    options: [{ id: '1', skin: 'danger', text: 'text', subtitle: 'subtitle' }],
   });
   instance.hideDropdown();
   instance.showDropdown();
