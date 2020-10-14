8d7c1c0eba8c933f15e5f5afc09aeb84a4d9001e
gendeld <danielge@wix.com>
2020-10-14 12:12:32 +0300

    Load more item if list height is bigger than initial rendered items

--- src/utils/InfiniteScroll.js
+++ src/utils/InfiniteScroll.js
@@ -135,6 +135,9 @@ export default class InfiniteScroll extends Component {
       scrollEl.removeEventListener('resize', this.scrollListener);
       this.detachScrollListener = () => {};
     };
+
+    // If there is space for more items, load them
+    this.scrollListener();
   }
 
   detachScrollListener = () => {};
